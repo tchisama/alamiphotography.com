cce8c20d4b2657ea9ed7550a0469f49338a8b9d5
tchi-sama <tchisama41@gmail.com>
2023-10-13 07:06:50 -0700

    adding shadcn to the app

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,60 @@
+"use client"
+import Image from 'next/image'
 import React from 'react'
+import blackLogo from "@/public/blacklogo.png"
+import Link from 'next/link'
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuIndicator,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    NavigationMenuViewport,
+  } from "@/components/ui/navigation-menu"
+import { Button } from './ui/button'
 
 type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <div>
-        hello world
+    <div className='w-full p-4  items-center flex justify-between'>
+        <Image alt='' src={blackLogo} width={160} height={100}></Image>
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={"uppercase px-2"}>Home</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={"uppercase px-2"}>About</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={"uppercase px-2"}>PORTFOLIO</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={"uppercase px-2"}>FOR PHOTOGRAPHERS</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={"uppercase px-2"}>CONTACT</NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                            <Button className={"ml-2"}>ENQUIRE</Button>
+                </NavigationMenuItem>
+
+            </NavigationMenuList>
+        </NavigationMenu>
+
     </div>
   )
 }
