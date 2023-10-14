6beebe15a7cbef6b53a900e8d2c471c4f3833345
tchi-sama <tchisama41@gmail.com>
2023-10-14 12:12:14 -0700

    format

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,12 +1,12 @@
-import { Loader } from 'lucide-react'
-import React from 'react'
+import { Loader } from "lucide-react";
+import React from "react";
 
 function Loading() {
   return (
-    <div className='flex h-screen items-center justify-center gap-2'>
-        loading <Loader className='animate-spin'/>
+    <div className="flex h-screen items-center justify-center gap-2">
+      loading <Loader className="animate-spin" />
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
