b2f7fc670420993e7923d5317ffc097722306b50
tchi-sama <tchisama41@gmail.com>
2023-11-01 04:02:09 -0700

    sign in

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,25 +6,58 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { LogIn } from 'lucide-react'
 import { useRouter } from 'next/navigation'
+import {AiOutlineGoogle} from "react-icons/ai"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '@/firebase'
+
+
 type Props = {}
 
 function Page({}: Props) {
   const router = useRouter()
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  const SignIn = () => {
+    setLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+        router.push("/dashboard");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setLoading(false)
+        setError(errorMessage);
+        console.log(errorCode, errorMessage);
+      });
+  }
   return (
-    <div className='min-h-screen flex items-center justify-center bg-primary'>
+    <form onSubmit={(e) => e.preventDefault()} className='min-h-screen flex items-center justify-center bg-[#fafaf8]'>
         <div className='w-full flex-1 gap-2 max-w-xl p-8 flex flex-col  items-center bg-[#fafaf8]'>
-            <Image src={logo} alt='' className='mb-8' width={200}></Image>
+            <Image src={logo} alt='' className='mb-8' width={300}></Image>
             <div className='w-full'>
-                <h4 className='my-2'>UserName</h4>
-                <Input></Input>
+                <h4 className='my-2'>Email</h4>
+                <Input placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}></Input>
             </div>
             <div className='w-full'>
                 <h4 className='my-2'>Password</h4>
-                <Input></Input>
+                <Input placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} type={"password"}></Input>
             </div>
-            <Button onClick={() => {router.push("/dashboard")}} className='w-full mt-4 text-lg flex gap-2'>Login<LogIn size={20}/></Button>
+            <div className='flex w-full mt-4 space-x-2'>
+              {/* <Button ><AiOutlineGoogle size={20}/></Button> */}
+              <Button type='submit' onClick={SignIn} className=' flex-1 text-lg flex gap-2'>Login<LogIn size={20}/></Button>
+            </div>
+            
         </div>
-    </div>
+    </form>
   )
 }
 
--- app/dashboard/blogs/page.tsx
+++ app/dashboard/blogs/page.tsx
@@ -1,10 +1,40 @@
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { Edit, Edit2, Plus } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>page</div>
+    <div className='px-2 '>
+        <div className='container mx-auto space-y-4'>
+
+            <h1 className='text-5xl my-8 '>Blogs</h1>
+            <Separator/>
+            <Button className='space-x-2'>Add Blog <Plus size={16}/></Button>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
+              <BlogComp/>
+              <BlogComp/>
+              <BlogComp/>
+              <BlogComp/>
+            </div>
+        </div>
+    </div>
+  )
+
+}
+
+const BlogComp = (props: Props) => {
+  return (
+    <Link href={"/blog/0"} className='flex group relative min-h-[300px] hover:bg-[#fff]  hover:scale-[1.01] flex-col gap-4 items-center pb-4 md:pb-8  duration-300'>
+        <img className='w-full aspect-square object-cover group-hover:shadow-none duration-300 ' src='https://www.niallscullyphotography.com/wp-content/uploads/2023/10/Michelle-Darragh-681-768x1154.jpg' alt=''></img>
+        <h3 className='md:text-xl text-center'>Wedding</h3>
+        <h2 className='text-xl md:text-2xl text-center'>Top Wedding Stationary Suppliers</h2>
+        <h4 className=' md:text-lg text-muted-foreground text-center'>September 15, 2023</h4>
+        <Button variant={"outline"} className='absolute right-0 top-0' size={"icon"}><Edit /></Button>
+    </Link>
   )
 }
 
