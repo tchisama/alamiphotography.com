2e947f071259bf87057298d5072a7121323f0295
tchi-sama <tchisama41@gmail.com>
2023-11-03 08:27:55 -0700

    image viewer done

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 
-type Props = {}
+  
+type Props = {
+    className?: string,
+    src:string,
+    alt:string
+}
 
-function ImageViewer({}: Props) {
+function ImageViewer({className,src,alt}: Props) {
   return (
-    <div>
-        
-    </div>
+<Dialog >
+  <DialogTrigger >
+    <img src={src} className={className} alt={alt}></img>
+  </DialogTrigger>
+  <DialogContent className='p-0 w-full h-full  max-h-none max-w-none'>
+        <img src={src} className={"w-full h-full object-contain"} alt={alt}></img>
+  </DialogContent>
+</Dialog>
+
   )
 }
 
