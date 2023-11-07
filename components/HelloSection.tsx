62630948d595e6f53378e8999ed34eac570047ad
tchi-sama <tchisama41@gmail.com>
2023-11-07 02:32:07 -0800

    fix hello section

--- components/HelloSection.tsx
+++ components/HelloSection.tsx
@@ -17,8 +17,13 @@ function HelloSection({}: Props) {
                     Always quick with a joke and ready to get down to business, I knows how to be productive and have fun at the same time. I am a family man whose wife and kids are at the centre of my world and inspiration for everyday life.
                     <br/>    
                     Please take a look around the website, and I hope we connect soon!
+                    A fine art film and digital wedding photographer based out Wicklow, Ireland, I am someone who takes great pride in their work. A fan of superheroes and zombie flicks, I enjoy lazy Sundays, the calm, the relaxation, the way they seem to breeze on by.
+                    <br/>    
+                    Always quick with a joke and ready to get down to business, I knows how to be productive and have fun at the same time. I am a family man whose wife and kids are at the centre of my world and inspiration for everyday life.
+                    <br/>    
+                    Please take a look around the website, and I hope we connect soon!
             </p>
-            <Button className='text-xl' size={'lg'}>Learn More</Button>
+            {/* <Button className='text-xl' size={'lg'}>Learn More</Button> */}
             <p className='text-6xl text-end w-full fontroman'>Alami</p>
         </div>
         <img className='espect-[3/4] w-full max-h-[50vh] md:max-h-[70vh] max-w-[500px] object-cover  rounded-lg ' src="https://www.niallscullyphotography.com/wp-content/uploads/2022/12/Me-3-596x894.jpg"></img>
