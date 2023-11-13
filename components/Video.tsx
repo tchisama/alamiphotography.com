4920384398b394b2da15f19f4faf986dd064b6f3
tchi-sama <tchisama41@gmail.com>
2023-11-13 10:05:20 -0800

    video done

--- components/Video.tsx
+++ components/Video.tsx
@@ -5,7 +5,7 @@ type Props = {}
 const Video = (props: Props) => {
   return (
     <div className='my-20'>
-      <video src="https://saltatelier.com.au/wp-content/uploads/2023/03/Sydney-Wedding-Videography-Cover.mov" preload="auto" muted autoPlay loop playsInline className="w-full scale-[1.15] md:scale-[1.05] h-full"></video>
+      <video src="https://firebasestorage.googleapis.com/v0/b/alamiphotography-b75a1.appspot.com/o/alami%20site%20web.mp4?alt=media&token=3f473e71-0053-41f6-9173-175464c57f6a" preload="auto" muted autoPlay loop playsInline className="w-full scale-[1.15] md:scale-[1.05] h-full"></video>
     </div>
   )
 }
--- components/WeAreCinematicStyle.tsx
+++ components/WeAreCinematicStyle.tsx
@@ -6,16 +6,16 @@ type Props = {}
 const WeAreCinematicStyle = (props: Props) => {
   
   return (
-    <div className='flex min-h-[80vh] my-40 scale-[1.04]'>
-        <div className='flex flex-1 bg-[rgba(238,227,222,0.64)] py-4 items-center justify-center'>
+    <div className='flex bg-white md:flex-row flex-col gap-2 md:gap-0 min-h-[80vh] my-20 md:my-40 md:scale-[1.04]'>
+        <div className='flex flex-1 md:bg-[rgba(238,227,222,0.64)] md:py-4 items-center justify-center'>
             <img className='flex-1 max-w-sm aspect-[2/3] my-12' src={"https://www.niallscullyphotography.com/wp-content/uploads/2023/03/Confetti-1st-Edits-8-597x894.jpeg?x15971"} alt="" />
         </div>
-        <div className='flex-[2] min-h-[80vh] flex flex-col bg-white justify-between'>
-            <h1 className='text-3xl md:text-7xl font-bold translate-y-12 -translate-x-8'>
+        <div className='flex-[2] md:min-h-[80vh] p-4 flex flex-col bg-white justify-between'>
+            <h1 className='text-2xl md:text-7xl font-bold md:translate-y-12 md:-translate-x-8'>
                     WE ARE CINEMATIC STYLE <br/>
                     STORYTELLERS.
             </h1>
-            <p className='text-2xl my-8 p-20 pl-32'>
+            <p className='text-lg md:text-2xl my-8  md:p-20 md:pl-32'>
               Every wedding has its own unique sense of style and personality, our cinematic approach is to be non-directorial and more observative while making our clients feel inspired, relaxed, and connected.
               We use the formula of documentary style shooting as it happens so that our works are honest,
               fun, and timeless. We want you to remember your day… not us directing you!
