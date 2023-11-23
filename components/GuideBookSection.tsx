ccc62c77cb13ed7e118c0766c9088f45aa5ad9b4
tchisama <tchisama41@gmail.com>
2023-11-22 23:41:40 -0800

    message send now

--- components/GuideBookSection.tsx
+++ components/GuideBookSection.tsx
@@ -5,20 +5,49 @@ import { Separator } from './ui/separator'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import Image from 'next/image'
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { db } from '@/firebase'
+import { toast } from './ui/use-toast'
 
 type Props = {}
 
 const GuideBookSection = (props: Props) => {
+  const [form, setForm] = React.useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+  const [loading, setLoading] = React.useState(false);
+  const submit =async () => {
+    setLoading(true)
+    await addDoc(collection(db, "guidebook"), {
+      name: form.name,
+      email: form.email,
+      message: form.message,
+      createdAt: serverTimestamp()
+    }).then(() => {
+      setLoading(false)
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      })
+      toast({
+        description: "Your message has been sent. Thank you!",
+        title: "Success",
+      })
+    })
+  }
   return (
     <div className='flex gap-10 mt-32 items-center'>
         <div className='flex-[4] flex flex-col gap-4 items-center text-center'>
             <h1 className='text-5xl'>I&apos;LL SEND YOU A GUIDE WITH EVERYTHING YOU NEED TO KNOW</h1>
             <p className='text-lg font-sans'>COUPLES THAT BOOK ME, GET MY WEDDING EXPERIENCE GUIDE FREE! THIS GUIDE WILL HELP YOU MAKE THE MOST OF YOUR WEDDING DAY WITH TIPS ON FIRST LOOK, DETAILS, TIMINGS, PORTRAITS AND MUCH MORE.</p>
             <Separator/>
-            <Input className='font-sans' placeholder='Name' />
-            <Input className='font-sans' placeholder='Email' />
-            <Textarea className='font-sans' placeholder="I'm open to any special requests."/>
-            <Button className='text-lg pt-7 uppercase tracking-wider flex gap-4 items-center'>Get Now <ArrowRight/></Button>
+            <Input value={form.name} onChange={(e) => setForm({...form, name: e.target.value})} className='font-sans' placeholder='Name' />
+            <Input value={form.email} onChange={(e) => setForm({...form, email: e.target.value})} className='font-sans' placeholder='Email' />
+            <Textarea value={form.message} onChange={(e) => setForm({...form, message: e.target.value})} className='font-sans' placeholder="I'm open to any special requests."/>
+            <Button disabled={loading} onClick={submit} className='text-lg pt-7 uppercase tracking-wider flex gap-4 items-center'>Get Now <ArrowRight/></Button>
         </div>
         <div className='flex-[4]'>
             <p className='text-center text-2xl'>From the moment you book me and up to your last image delivered, you have my full undivided attention, energy and care. I can help you fine tune the schedule of your wedding, so we catch the best light for intimate portraits. I&apos;ve worked with tons of vendors over the years and can offer a recommendation if you still haven&apos;t found a florist, make up artist or candy bar provider.</p>
