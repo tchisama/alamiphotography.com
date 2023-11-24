de89c1f911e57a9b38319bf3b3ba4a2f998aca51
tchisama <tchisama41@gmail.com>
2023-11-24 07:49:57 +0100

    added the steps section

--- components/Steps.tsx
+++ components/Steps.tsx
@@ -4,19 +4,17 @@ type Props = {}
 
 const Steps = (props: Props) => {
   return (
-    <div className='w-full gap-2 grid grid-cols-4'>
+    <div className='w-full gap-10 grid grid-cols-4 my-20 py-24'>
         {
             ["we chat & see if we're a good fit.","you pay a deposit & sign a contract.","i photograph your wedding.","your gallery is delivered."]
             .map((step, index) => (
-                <div className='flex flex-col items-center gap-2' key={index}>
-                    <div className='text-8xl fontroman text-[#a9b9b8]'>{index+1}</div>
-                    <div className='text-md fontcharm text-[#a9b9b8]'>{step}</div>
+                <div className='flex border-y py-8 flex-col items-center gap-8' key={index}>
+                    <div className='text-2xl font-mono uppercase text-[#a9b9b8]'>step 0{index+1}</div>
+                    <div className='text-4xl text-center fontroman text-[#a9b9b8]'>{step}</div>
                 </div>
             ))
         }
         <div>
-            <div>STEP 01</div>
-            <div>we chat & see if we{"'"}re a good fit.</div>
         </div>
     </div>
   )
