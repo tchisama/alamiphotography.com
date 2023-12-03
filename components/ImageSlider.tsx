8d999a55757ce264f156f1b217a6a1a08fcd11e4
tchisama <tchisama41@gmail.com>
2023-12-03 18:16:32 +0100

    fix l9lawi

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -99,7 +99,27 @@ const ImageSlider = (props: Props) => {
         }
     </div>
         <div>
-            <SliderButtons setCurrent={setCurrent} current={current} images={images.length}/>
+        <div className="flex gap-4  mx-auto   items-center justify-between max-w-[250px] md:max-w-md img mt-2 md:mt-8">
+      <button onClick={() => setCurrent(current==0?0:current - 1)} className="text-primary flex items-center p-2 group" >
+        <ArrowLeft/>
+        <div className="w-[30px] md:w-[50px] h-[2px]  duration-200  -translate-x-2 bg-primary">
+        </div>
+        </button>
+        {
+          current > 0 ? (
+            <div className="text-sm md:text-lg">{(((current % images.length)??0) + 1)} / {images.length}</div>
+          )
+          :
+          (
+            <div className="text-sm md:text-lg">{1} / {images.length}</div>
+          )
+        }
+      <button onClick={() => setCurrent(current + 1)} className="text-primary group flex items-center p-2" >
+        <div className="w-[30px] md:w-[50px] h-[2px]  duration-200 translate-x-2 bg-primary">
+        </div>
+        <ArrowRight/>
+      </button>
+    </div>
         </div>
     </>
   );
