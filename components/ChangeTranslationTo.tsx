042fea1edb5a92abab7220d8443bdd1c2b856309
tchisama <tchisama41@gmail.com>
2023-12-20 13:22:13 +0100

    asdfasd

--- components/ChangeTranslationTo.tsx
+++ components/ChangeTranslationTo.tsx
@@ -3,9 +3,10 @@ import React, { useEffect, useState } from 'react'
 import { Input } from './ui/input'
 import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '@/firebase';
-import { Check, Coffee, Replace } from 'lucide-react';
+import { AlertTriangle, Check, Coffee, Replace } from 'lucide-react';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
+import { AlertTitle } from './ui/alert';
 
 
 
@@ -19,6 +20,10 @@ type Props = {
 function ChangeTranslationTo({text,lang}: Props) {
     const [config, setConfig] = useState<string>();
     const [edit, setEdit] = useState<boolean>(false)
+    const [data, setData] = useState<{en:string,fr:string}>({
+        en:"",
+        fr:""
+    })
     useEffect(() => {
         try {
             const unSub = onSnapshot(doc(db, "configs","translate"), (doc) => {
@@ -26,6 +31,7 @@ function ChangeTranslationTo({text,lang}: Props) {
                 if(!doc.data()[text]) return
                 if(!doc.data()[text][lang]) return setConfig("")
                 setConfig(doc.data()[text][lang] ?? "" as string)
+                setData({...data, [lang]:doc.data()[text]})
             })
             return () => unSub()
         } catch (error) {
@@ -34,6 +40,7 @@ function ChangeTranslationTo({text,lang}: Props) {
     },[text,lang])
     const changeText = () => {
         const updated = {
+            ...data,
             [lang]: config
         }
         updateDoc(doc(db, "configs","translate"), {
@@ -44,9 +51,17 @@ function ChangeTranslationTo({text,lang}: Props) {
     <>
         {
             edit ? <Textarea  value={config} onChange={(e) => setConfig(e.target.value)} className='w-full font-sans min-h-[200px] h-full font-medium' /> :
-            <span className='font-sans font-medium break-words'>{config=="" ? <span className='opacity-50 font-sans font-medium'>there is no value</span>: config}</span>
+            <span className='font-sans font-medium break-words'>{config=="" ? <span className='opacity-50 font-sans font-medium'>there is no value</span>: <span className='font-sans font-medium' dangerouslySetInnerHTML={{__html: config?.replace(/\n/g,"<br/>" ) as string}}></span> }</span>
         }
-
+        
+        {
+            !data?.en || !data?.fr ? (
+            <div className='bg-yellow-100 px-4 border border-yellow-400 text-yellow-700 p-2 text-sm rounded-xl w-fit flex gap-4 items-center font-sans'>
+                <AlertTriangle size={16}/>
+                You need to add {!data?.fr ? "french" : ""} - {!data?.en ? "english" : ""} translation
+            </div>
+            ) : null
+        }
         <Button onClick={() => {
             setEdit(!edit)
             if (edit) {
