8e2436470629c5bd19b410fa6dbfb717635d6988
tchisama <tchisama41@gmail.com>
2023-12-21 20:42:54 +0100

    finish translation in all pages

--- app/[locale]/wedding-film/page.tsx
+++ app/[locale]/wedding-film/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import AlamiSection from '@/components/AlamiSection'
 import Footer from '@/components/Footer'
+import GetText from '@/components/GetText'
 import Loading from '@/components/Loading'
 import Navbar from '@/components/Navbar'
 import VideosSlider from '@/components/VideosSlider'
@@ -42,15 +43,7 @@ const Page = (props: Props) => {
             {/* <h1 className="absolute md:text-white md:bottom-10 md:left-10 drop-shadow-2xl font-bold text-3xl md:text-6xl ">Capturing Timeless Beauty & Emotion</h1> */}
         </div>
         <div className='max-w-[1500px] mx-auto flex justify-center'>
-            <p className='text-center max-w-3xl mx-auto mt-4 md:py-28 text-sm p-4 md:text-xl'>
-                    &quot;Every wedding has its own unique sense of style and personality,
-
-                    our cinematic approach is to be non-directorial and more observative while making our clients feel inspired,
-
-                    relaxed, and connected. We use the formula of documentary style shooting as it happens so that our works are honest,
-
-                    fun, and timeless. We want you to remember your day… not us directing you!&quot;
-            </p>
+            <p className='text-center max-w-3xl mx-auto mt-4 md:py-28 text-sm p-4 md:text-xl'><GetText id="wedding_films_content"/></p>
         </div>
         <div className='py-24'>
             <VideosSlider/>
--- components/GuideBookSection.tsx
+++ components/GuideBookSection.tsx
@@ -8,6 +8,8 @@ import Image from 'next/image'
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 import { db } from '@/firebase'
 import { toast } from './ui/use-toast'
+import GetText from './GetText'
+import { useTranslations } from 'next-intl'
 
 type Props = {}
 
@@ -17,6 +19,7 @@ const GuideBookSection = (props: Props) => {
     email: "",
     message: "",
   })
+  const t = useTranslations('translation');
   const [loading, setLoading] = React.useState(false);
   const submit =async () => {
     setLoading(true)
@@ -41,16 +44,16 @@ const GuideBookSection = (props: Props) => {
   return (
     <div className='flex flex-col-reverse md:flex-row gap-10 mt-32 items-center'>
         <div className='flex-[4] flex flex-col gap-4 items-center text-center'>
-            <h1 className='text-2xl md:text-5xl'>I&apos;LL SEND YOU A GUIDE WITH EVERYTHING YOU NEED TO KNOW</h1>
-            <p className='text-sm md:text-lg font-sans'>COUPLES THAT BOOK ME, GET MY WEDDING EXPERIENCE GUIDE FREE! THIS GUIDE WILL HELP YOU MAKE THE MOST OF YOUR WEDDING DAY WITH TIPS ON FIRST LOOK, DETAILS, TIMINGS, PORTRAITS AND MUCH MORE.</p>
+            <h1 className='text-2xl md:text-5xl'><GetText id="experiencePage_guide_title"/></h1>
+            <p className='text-sm md:text-lg font-sans'><GetText id="experiencePage_guide_subtitle"/></p>
             <Separator/>
-            <Input value={form.name} onChange={(e) => setForm({...form, name: e.target.value})} className='font-sans' placeholder='Name' />
-            <Input value={form.email} onChange={(e) => setForm({...form, email: e.target.value})} className='font-sans' placeholder='Email' />
-            <Textarea value={form.message} onChange={(e) => setForm({...form, message: e.target.value})} className='font-sans' placeholder="I'm open to any special requests."/>
+            <Input value={form.name} onChange={(e) => setForm({...form, name: e.target.value})} className='font-sans' placeholder={t("experiencePage_guide_name")} />
+            <Input value={form.email} onChange={(e) => setForm({...form, email: e.target.value})} className='font-sans' placeholder={t('experiencePage_guide_email')} />
+            <Textarea value={form.message} onChange={(e) => setForm({...form, message: e.target.value})} className='font-sans' placeholder={t("experiencePage_guide_i'm_open_to_any_request")}/>
             <Button disabled={loading} onClick={submit} className='text-sm md:text-lg pt-7 uppercase tracking-wider flex gap-4 items-center'>Get Now <ArrowRight size={18}/></Button>
         </div>
         <div className='flex-[4]'>
-            <p className='md:text-start text-sm text-center md:text-lg'>From the moment you book me and up to your last image delivered, you have my full undivided attention, energy and care. I can help you fine tune the schedule of your wedding, so we catch the best light for intimate portraits. I&apos;ve worked with tons of vendors over the years and can offer a recommendation if you still haven&apos;t found a florist, make up artist or candy bar provider.</p>
+            <p className='md:text-start text-sm text-center md:text-lg'><GetText id="experiencePage_guide_paragraph"/></p>
         </div>
         <div className='flex-[2]'>
             <Image width={500} height={750} className='w-full bg-[#0002]' src="https://firebasestorage.googleapis.com/v0/b/alamiphotography-b75a1.appspot.com/o/files%2F943996204%20SEND%20YOU%20A%20GUIDE.jpg?alt=media&token=141e01a2-8b2d-41a3-bfb5-d947c30429eb" alt="" />
